4c2470e320bd230f931282f44f6f84626d762fd1
RodrigoMato00 <85580712+RodrigoMato00@users.noreply.github.com>
2022-06-27 16:07:39 -0300

    declRndo componente a-frame

--- sintetizador.js
+++ sintetizador.js
@@ -1,14 +1,46 @@
-// Sintetizador
-const synth = new Tone.Synth({
-    volume: -12, // volumen del oscilador ajustado a -12dB
-    oscillator: {
-      type: 'square', // tipo de oscilador a onda cuadrada
+  AFRAME.registerComponent('sintetizador', {
+    schema: {
+      note: {
+        type: 'string',
+        default: 'C4',
+      },
+      duration: {
+        type: 'string',
+        default: '8n',
+      },
     },
-    envelope: {
-      attack: 0.02, //ajuste a 20ms
-      release: 1, // realse ajustado a 1s
+    init: function () {
+      this.el.addEventListener('fusing', this.trigger.bind(this))
     },
-  }).toMaster() //conectar la salida del sintetizador al filtro
+    trigger: function () {
+      synth.triggerAttackRelease(this.data.note, this.data.duration)
+    },
+    update: function () {},
+    tick: function () {},
+    remove: function () {},
+    pause: function () {},
+    play: function () {},
+  })
 
-  //indicar al sintetizador que reproduzca la nota Do3 durante 8
-  synth.triggerAttackRelease('C3', '8n')
+//efecto FeedbackDelay, que repite cada corchea con un 80% de feedback
+const delay = new Tone.FeedbackDelay('8n', 0.8)
+// encendido en un volumen ajustado a -12dB y luego a la salida Master
+.chain(new Tone.Volume(-12), Tone.Master)
+
+// filtro de paso bajo con una frecuencia de 1500 Hz
+const filter = new Tone.Filter(1500, 'lowpass')
+// La señal se envía tanto al Delay como al Master
+.connect(delay)
+.toMaster()
+
+// sintetizador
+const synth = new Tone.Synth({
+volume: -12, // volumen del oscilador ajustado a -12dB
+oscillator: {
+  type: 'square', // tipo de oscilador a onda cuadrada
+},
+envelope: {
+  attack: 0.02, // ajuste a 20ms
+  release: 1, // realse ajustado a 1s
+},
+}).connect(filter) // conectar la salida del sintetizador al filtro
